3b3185542ef61aedbb8730e23997444a945beb0c
riederdominik <rieder.dominik@gmx.at>
2021-03-09 08:07:21 +0100

    Updated Scripts

---
+++ Assets/MyGame/Scripts/Controller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controller : MonoBehaviour
+{
+
+    public void ChangeScene0to1()
+    {
+        SceneManager.LoadScene("01ColorScene");
+    }
+
+    public void ChanceScene1to2()
+    {
+        SceneManager.LoadScene("02NumberScene");
+    }
+
+    public void ChangeScene2to0()
+    {
+        SceneManager.LoadScene("00WelcomeScene");
+    }
+}
